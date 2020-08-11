47c02b3062885dc4e88f1d322ec2676e73a36cde
nearnshaw <nearnshaw@decentraland.org>
2020-08-11 16:26:52 -0300

    trigger sphere

--- src/game.ts
+++ src/game.ts
@@ -1,67 +1,89 @@
 import utils from '../node_modules/decentraland-ecs-utils/index'
 import { InterpolationType } from '../node_modules/decentraland-ecs-utils/transform/math/interpolation'
+import { TriggerSphereShape } from '../node_modules/decentraland-ecs-utils/triggers/triggerSystem'
 
-import { getUserAccount } from '@decentraland/EthereumController'
+let deflatedScale = new Vector3(0.05, 0.05, 0.065)
+let inflatedScale = new Vector3(0.11, 0.11, 0.075)
 
 let puffer = new Entity()
 
 puffer.addComponent(new GLTFShape('models/puffer.gltf'))
 
-let pufferTransform = new Transform()
-puffer.addComponent(pufferTransform)
-
-let deflatedScale = new Vector3(0.05, 0.05, 0.065)
-let inflatedScale = new Vector3(0.11, 0.11, 0.075)
-
-pufferTransform.position.set(8, 1, 8)
-pufferTransform.scale = deflatedScale
+puffer.addComponent(
+  new Transform({
+    position: new Vector3(8, 1, 8),
+    scale: deflatedScale,
+  })
+)
 
 let isInflating = false
 
+// sound when deflating
 let deflatedSound = new AudioClip('sounds/deflate.wav')
 puffer.addComponent(new AudioSource(deflatedSound))
 
 engine.addEntity(puffer)
 
+// Reaction when clicked
 puffer.addComponent(
   new OnPointerDown(
-    e => {
-      if (isInflating) return
-      isInflating = true
-      puffer.addComponent(
-        new utils.ScaleTransformComponent(
-          deflatedScale,
-          inflatedScale,
-          1,
-          null,
-          InterpolationType.EASEINQUAD
-        )
-      )
-      puffer.addComponent(
-        new utils.Delay(2000, () => {
-          puffer.getComponent(AudioSource).playOnce()
-          puffer.addComponent(
-            new utils.ScaleTransformComponent(
-              inflatedScale,
-              deflatedScale,
-              3,
-              () => {
-                isInflating = false
-              }
-            )
-          )
-        })
-      )
+    (e) => {
+      inflateFish()
     },
     { button: ActionButton.POINTER, hoverText: 'Puff up!' }
   )
 )
 
+// trigger when player walks near fish
+puffer.addComponent(
+  new utils.TriggerComponent(
+    new TriggerSphereShape(2, Vector3.Zero()),
+    null,
+    null,
+    null,
+    null,
+    () => {
+      inflateFish()
+    }
+  )
+)
+
+/// Reusable function to inflate fish, called both by the click and the trigger
+function inflateFish() {
+  if (isInflating) return
+  isInflating = true
+  puffer.addComponent(
+    new utils.ScaleTransformComponent(
+      deflatedScale,
+      inflatedScale,
+      1,
+      null,
+      InterpolationType.EASEINQUAD
+    )
+  )
+  puffer.addComponent(
+    new utils.Delay(2000, () => {
+      puffer.getComponent(AudioSource).playOnce()
+      puffer.addComponent(
+        new utils.ScaleTransformComponent(
+          inflatedScale,
+          deflatedScale,
+          3,
+          () => {
+            isInflating = false
+          }
+        )
+      )
+    })
+  )
+}
+
+/// Ground
 let ground = new Entity()
 engine.addEntity(ground)
 ground.addComponent(new GLTFShape('models/FloorBaseDirt_01.glb'))
 ground.addComponent(
   new Transform({
-    position: new Vector3(8, 0, 8)
+    position: new Vector3(8, 0, 8),
   })
 )
